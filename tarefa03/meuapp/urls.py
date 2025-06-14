819ec928c43c71cf3724e4b4aee56de7a76473ac
pedro <pedruhenrique56@gmai.com>
2025-06-14 03:05:47 -0300

    tarefa03 COMPLETA

--- tarefa03/meuapp/urls.py
+++ tarefa03/meuapp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.inicio, name="inicio"),
+    path("usuarios/", views.usuarios, name="usuarios")
 ]
+
+#para criar um novo "path", uma URL para um nova página, é necessário colocar específicada no inicio: 
+#path({colocar aqui, entre aspas}, ... (como está em usuários)
+#OBS: se por o nome do arquivo exato ("usuarios.html"; "inicio.html") também funcionará.
+#(inclusive eu não sei porque o inicio, que seria o 'index', não precisa disso)
--- tarefa03/meuapp/views.py
+++ tarefa03/meuapp/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, "inicio.html")
 
+def usuarios(request):
+    user_list = [
+        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
+        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
+        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
+        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
+        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
+    ]
+
+    context = {
+        "usuarios": user_list,# "{qualquer nome}": {atributo que você deseja dar como reposta}. isso é um dicionário
+    }
+    return render(request, "usuarios.html", context)
+
 #aqui é onde é colocado oque a página necessitará. 
 #escrito no slide: Recebem e processam as requisições, retornando um conteúdo baseado nos templates
 #lembrar de também mudar o nome da função!!!!!!!!!!!!!!!!!
