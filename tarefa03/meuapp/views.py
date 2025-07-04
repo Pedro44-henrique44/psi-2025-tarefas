0ce72626e2858f4da370effd170ac396409e363b
pedro <pedruhenrique56@gmai.com>
2025-07-04 19:15:24 -0300

    mudando as pessoas na "views"

--- tarefa03/meuapp/views.py
+++ tarefa03/meuapp/views.py
@@ -5,11 +5,11 @@ def inicio(request):
 
 def usuarios(request):
     user_list = [
-        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
-        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
-        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
-        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
-        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "19451945"},
+        {"nome": "Diego", "idade": "30", "cidade": "Natal", "matricula": "20101001110029"},
+        {"nome": "Pedro", "idade": "17", "cidade": "SPP", "matricula": "20231181110004"},
+        {"nome": "Chico", "idade": "21", "cidade": "Natal", "matricula": "20201181110023"},
+        {"nome": "Dede", "idade": "27", "cidade": "Natal", "matricula": "20161181110031"},
+        {"nome": "Xuao", "idade": "56", "cidade": "Natal", "matricula": "19991181110001"},
     ]
 
     context = {
